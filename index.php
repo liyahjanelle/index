5e238bf03368d0e5a5cadd3a8100ec6379925d97
liyahjanelle <80090323+liyahjanelle@users.noreply.github.com>
2021-03-11 23:28:44 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head><title> Challenge 6- table 1 with COLSpan </title></head>
+<body>
+
+<table border = 1>
+<tr BGCOLOR = YELLOW>
+
+
+<tH COLSPAN = "4"> <CENTER> STUDENT'S PERSONAL INFORMATION </CENTER> </TH></TR>
+
+<TR BGCOLOR = YELLOW>
+<TH> NAME OF STUDENT </TH>
+<TH> BIRTHDATE </TD>
+<TH> AGE </TD>
+<TH COLSPAN = 2> <CENTER> PARENT'S NAME </CENTER> </TH></TR>
+
+<tr BGCOLOR = YELLOW>
+<td> Coronia Versoza </TD>
+<TD> March 01, 2000 </TD>
+<TD> 20 </TD>
+<TD> Mrs. Pandemia </TD>
+<TD> Mr. Quarantino </TD>
+</TR>
+<TR BG COLOR = YELLOW>
+
+<td> Faith Covida </TD>
+<TD> May 06, 2002 </TD>
+<TD> 18 </TD>
+<TD> Mrs. Hope </TD>
+<TD> Mr. John </TD>
+
+</TR>
+</table>
+</body>
+</html>
